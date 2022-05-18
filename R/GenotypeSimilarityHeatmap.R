a995c5cbfe7499fbf87b9eaa0537529c91fbc0c3
Nicolas Broguiere <broguiere.nicolas@gmail.com>
2022-05-18 15:09:15 +0200

    Renamed GenotypeMatchingFractionPlot to GenotypeSimilarityHeatmap

--- R/GenotypeSimilarityHeatmap.R
+++ R/GenotypeSimilarityHeatmap.R
@@ -13,10 +13,10 @@
 #' @return A heatmaply plot.
 #' @keywords genotypes variants vcf
 #' @examples
-#' GenotypeMatchingFractionPlot(MyGenotypeObject)
+#' GenotypeSimilarityHeatmap(MyGenotypeObject)
 #' @export
 
-GenotypeMatchingFractionPlot <- function(genotypes, key.title="Fraction of \nmatching mutations", limits=NULL, plot_method="plotly", colors = viridis(n = 256, alpha = 1, begin = 0, end = 1, option = "viridis"), ...){
+GenotypeSimilarityHeatmap <- function(genotypes, key.title="Fraction of \nmatching mutations", limits=NULL, plot_method="plotly", colors = viridis(n = 256, alpha = 1, begin = 0, end = 1, option = "viridis"), ...){
   # Very important to handle the sparse matrices carefully to get it to run fast. Could get a further factor 2 by only computing half of the symmetrical matrix...
   tmp_matching <- as.matrix(t(genotypes@matrix==1) %*% (genotypes@matrix==1))+as.matrix(t(genotypes@matrix==2) %*% (genotypes@matrix==2))+as.matrix(t(genotypes@matrix==3) %*% (genotypes@matrix==3))
   tmp_overlap <- as.matrix(t(genotypes@matrix>0) %*% (genotypes@matrix>0))
