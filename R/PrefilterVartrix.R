4182b22ddfb0a896ebefddd94566cf8f73c50c52
Nicolas Broguiere <broguiere.nicolas@gmail.com>
2022-05-18 14:44:43 +0200

    Added PrefilterVartrix

--- R/PrefilterVartrix.R
+++ R/PrefilterVartrix.R
@@ -2,12 +2,14 @@
 #'
 #' Given a list of vartrix matrices, and a minimal number of cells/barcodes for which variant data should be available, returns a list of vartrix matrices with filtered variants.
 #'
+#' The row order and rownames are kept consistent with the input and across all matrices during the subsetting.
+#'
 #' @param vartrix list of dgCmatrix, a list of vartrix matrices (containg at least a consensus matrix, named as such, in order to compute coverage).
 #' @param min.cells numeric(1) the minimum number of cells for which there should be a genotype call for the variants to be kept.
 #' @return A list of vartrix matrices in the same format as the input
 #' @keywords genotypes variants vcf
 #' @examples
-#' GenotypeMatchingFractionPlot(MyGenotypeObject)
+#' MyVartrixMatrices <- PrefilterVartrix(MyVartrixMatrices, min.cells=10)
 #' @export
 
 PrefilterVartrix <- function(vartrix_matrices,min.cells=5){
