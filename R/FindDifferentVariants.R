dda53517739512267a86ac4b8213487957459655
Nicolas Broguiere <broguiere.nicolas@gmail.com>
2022-06-17 16:19:31 +0200

    Packaged differential frequency testing and pseudo bulk genotype aggregation functions.

---
+++ R/FindDifferentVariants.R
@@ -0,0 +1,99 @@
+#' Find differentially frequent variants between two populations
+#'
+#' Starting from a Seurat object, and two groups of cells (given as their ident name, or as a vector of cell barcodes. If second group is omitted, assumed to be all other cells), computes the differential frequency of occurance of variants with a chi-square frequency test. Both p-values and Bonferroni-corrected adjusted p-values are returned, as well as the difference of frequency of alternate alleles between the two populations.
+#'
+#' @param object Seurat object.
+#' @param ident.1 character(n). The name of an identity or a list of cell barcodes or column positions in the Seurat object, defining the first cell population.
+#' @param ident.2 character(n). The name of an identity or a list of cell barcodes or column positions in the Seurat object, defining the second cell population, or NULL to use all the other cells. Default: NULL.
+#' @param min.cells numeric(1). The minimal number of cells that should be have data in both populations for differential testing to be performed. Default: 15.
+#' @param sort.by character(1). "pval" to sort the result dataframe by p-value, or "deltafreq" to sort by absolute difference in frequency. Any other value will result in no sorting, conserving the feature order in the Seurat object. Default: pval.
+#' @param assays character(2). The name of the assays in which the reference allele count and alt allele count are stored in the seurat object. Default: c("REF","ALT").
+#' @param slot character(1). The assay slot that should be used in the Seurat object. Default: "data".
+#' @param show.progress numeric(1). Number of tests performed between updates on the advance. FALSE or 0 for no progress reports. Default: 500.
+#' @param pval.lower.limit numeric(1). p-vals lower than this limit will be truncated to this limit. Useful to avoid log(0) and set an axis limit in volcano-like plots of the results.
+#' @return Returns a data frame with columns "n1" (number of cells with variant data in population 1) and "n2" (same in population 2), the frequency of ref and alt allele in both populations "freq1" and "freq2", the difference in frequency "deltafreq", and the results of the chi-square test for identitical frequency with or without Bonferroni correction "pval" and "adj.pval".
+#' @keywords differentially frequent variants statistical testing
+#' @export
+#' @examples
+#' DifferentialTestingresults <- FindDifferentVariants(MySeuratObject, "FirstIdentName", "OtherIdentName")
+FindDifferentVariants <- function(object, ident.1, ident.2=NULL, min.cells=15, sort.by="pval", assay=c("REF","ALT"), slot="data", show.progress=500, pval.lower.limit=1e-100){
+  # Clarify the identities to use
+  if(length(ident.1)==1){
+    ident.1 <- colnames(object)[Idents(object)==ident.1]
+  }
+  if(length(ident.2)==1){
+    ident.2 <- colnames(object)[Idents(object)==ident.2]
+  }else if(is.null(ident.2)){
+    ident.2 <- setdiff(colnames(object),ident.1)
+  }
+
+  if(!length(ident.1)){
+    warning("ident.1 is empty. Aborting.")
+    return(object)
+  }
+  if(!length(ident.2)){
+    warning("ident.2 is empty. Aborting.")
+    return(object)
+  }
+  # Pick up the data filtered above min.cell threshold, and aggregate the counts of ref and alt:
+  variants_ident_1_ref <- GetAssayData(object, assay=assay[1], slot=slot)[,ident.1]
+  variants_ident_1_alt <- GetAssayData(object, assay=assay[2], slot=slot)[,ident.1]
+  variants_ident_2_ref <- GetAssayData(object, assay=assay[1], slot=slot)[,ident.2]
+  variants_ident_2_alt <- GetAssayData(object, assay=assay[2], slot=slot)[,ident.2]
+  tmp1 <- Matrix::rowSums((variants_ident_1_ref+variants_ident_1_alt)>0)
+  tmp2 <- Matrix::rowSums((variants_ident_2_ref+variants_ident_2_alt)>0)
+  keep <- tmp1>min.cells & tmp2>min.cells
+  variant.names <- rownames(variants_ident_1_ref)[keep]
+  variants_ident_1_ref <- Matrix::rowSums(variants_ident_1_ref[keep,])
+  variants_ident_1_alt <- Matrix::rowSums(variants_ident_1_alt[keep,])
+  variants_ident_2_ref <- Matrix::rowSums(variants_ident_2_ref[keep,])
+  variants_ident_2_alt <- Matrix::rowSums(variants_ident_2_alt[keep,])
+
+  # Prepare the dataframe that will collect the differential testing results
+  difftest <- data.frame(row.names = variant.names)
+  difftest$n1 <- NA
+  difftest$n2 <- NA
+  difftest$freq1 <- NA
+  difftest$freq2 <- NA
+  difftest$deltafreq <- NA
+  difftest$pval <- NA
+  difftest$adj.pval <- NA
+
+  # Loop through the variants and perform chisquare tests
+  if(show.progress){
+    cat("Progress:\n")
+  }
+  for(i in 1:nrow(difftest)){
+    xref <- variants_ident_1_ref[i]
+    xalt <- variants_ident_1_alt[i]
+    yref <- variants_ident_2_ref[i]
+    yalt <- variants_ident_2_alt[i]
+    contingency_table <- matrix(c(xref,xalt,yref,yalt), nrow = 2, ncol = 2, byrow = T, dimnames = list(c("x","y"),c("REF","ALT")))
+    Xsq <- stats::chisq.test(contingency_table, correct = F)
+
+    difftest$n1[i] <- xref+xalt
+    difftest$n2[i] <- yref+yalt
+    difftest$freq1[i] <- xalt/(xref+xalt)
+    difftest$freq2[i] <- yalt/(yref+yalt)
+    difftest$pval[i] <- Xsq$p.value
+
+    if(show.progress){
+      if(!i%%show.progress){
+        print(paste0(i," out of ",nrow(difftest)))
+      }
+    }
+  }
+  difftest$deltafreq <- difftest$freq1 - difftest$freq2
+  difftest$adj.pval  <- pmin(1,difftest$pval * nrow(difftest))
+
+  difftest$pval     <- pmax(pval.lower.limit,difftest$pval)
+  difftest$adj.pval <- pmax(pval.lower.limit,difftest$adj.pval)
+
+  if(sort.by[1]=="deltafreq"){
+    difftest <- difftest[order(-abs(difftest$deltafreq)),] # sort by difference in frequency
+  }else if(sort.by[1]=="pval"){
+    difftest <- difftest[order(difftest$pval),] # sort by significance
+  }
+
+  return(difftest)
+}
---
+++ R/PseudoBulkGenotypes.R
@@ -0,0 +1,75 @@
+#' Aggregate pseudo-bulk genotypes
+#'
+#' Starting from a Seurat object, for each of the identities given as the ident argument, compute a pseudo-bulk aggregated genotype. Store the results in a genotype object. If the ident is a list of cells, compute only one aggregated genotype for the cells listed. If it is the name of a metadata column, or a vector listing the identity of each individual cells, use the levels of this vector/column to define the groups of cells aggregated into individual pseudo-bulk genotypes.
+#'
+#' @param object Seurat object.
+#' @param ident character(n). The name of a metadata column, or a vector listing the individual identities of cells, or a list of cells. Defines the groups of cells that are aggregated in pseudo-bulk genotypes. Default: Idents(object).
+#' @param tolerance.pct numeric(1). The percentage of counts not matching the rest tolerated to keep a homozygous call.
+#' @param assays character(3). The name of the assays in which the consensus matrix (expected to also contain variant metadata), reference allele count and alt allele count are stored in the seurat object. Default: c("VAR","REF","ALT").
+#' @param slot character(1). The assay slot that should be used in the Seurat object. Default: "data".
+#' @return Returns a genotype object with the aggregated pseudo-bulk genotypes and their metadata.
+#' @keywords pseudo-bulk Pseudobulk genotype aggregation
+#' @export
+#' @examples
+#' GenotypeObject <- PseudoBulkGenotypes(MySeuratObject)
+PseudoBulkGenotypes <- function(object, ident=Idents(object), tolerance.pct=5, assays=c("VAR","REF","ALT"), slot="data"){
+  # Clarify the identities to use
+  if(!length(ident)){
+    print("No cluster identity given, using default Idents(object)")
+    idents <- Idents(object)
+  }else{
+    if(is.na(ident[1]) | ident==Idents(object)){
+      print("Using the levels of Idents(object) for pseudo-bulk genotype aggregation.")
+      idents <- Idents(object)
+    }else{
+      if(length(ident)==1){
+        print("Using metadata column",ident,"as identities for the cluster genotypes.")
+        idents <- object@meta.data[,ident,drop=T]
+      }else{
+        if(length(intersect(ident,colnames(object)))==length(ident)){
+          print("Using the list of cell barcodes given to aggregate a unique genotype.")
+          object <- object[,ident]
+          Idents(object) <- "GT"
+        }else if(length(ident)==ncol(object)){
+          print("Using the levels of the identities given to define the clusters aggregated into genotypes.")
+          idents <- ident
+        }else{
+          warning("Could not make sense of the ident given as input. Give a vector of cell names, a metadata column, a set of identities of the same length as ncol(object), or NA/NULL to use current Idents(object). Aborting.")
+          return()
+        }
+      }
+    }
+  }
+  # Names of the genotypes:
+  gt.names <- as.character(unique(idents))
+
+  # Pick up the variant metadata as is from the seurat object:
+  meta <- object[[assays[1]]]@meta.features
+
+  # Prepare a genotype sparse matrix to receive the data (vartrix conventions):
+  m <- Matrix::Matrix(data = 0, nrow = nrow(object), ncol = length(gt.names), dimnames = list(rownames(object),gt.names))
+  m <- methods::as(m,"dgCMatrix")
+
+  # Go through the clusters, pick up the data, aggregate, compute coverage, frequency, and make a genotype call:
+  tol <- tolerance.pct/100
+  for(i in gt.names){
+    ref <- Matrix::rowSums(GetAssayData(object, assay=assays[2], slot=slot)[,idents==i])
+    alt <- Matrix::rowSums(GetAssayData(object, assay=assays[3], slot=slot)[,idents==i])
+    cvg <- ref+alt
+    freq <- alt/cvg + 1 # Offset by one to distinguish no data from 0 frequency
+    freq[cvg==0] <- 0
+    gt <- ref # Just use ref as a template for the shape
+    gt[] <- 0 # No data unless overwritten
+    gt[cvg>0] <- 3 # ref/alt unless overwritten
+    gt[freq >=1 & freq<=1+tol] <- 1 # ref/ref
+    gt[freq>=2-tol] <- 2 # alt/alt
+
+    # fill in the matrices
+    meta[,paste0("coverage_",i)] <- cvg
+    m[,i] <- gt
+    m <- Matrix::drop0(m)
+  }
+
+  # Package the results in a genotype object to return:
+  return(CreateGenotypeObject(matrix = m, metadata = meta, variants = rownames(m), informative_variants = VariableFeatures(object, assay=assays[1])))
+}
