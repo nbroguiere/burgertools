7f69daa82e1cd0418619dd62e95e79c5d0255cdd
Nicolas Broguiere <broguiere.nicolas@gmail.com>
2022-06-17 15:15:27 +0200

    Make a CreateGenotypeObject function to bypass the problems to export the default constructor.

--- R/GenotypeObject.R
+++ R/GenotypeObject.R
@@ -24,7 +24,7 @@
 #' @slot variants_by_information A character vector listing the variants sorted from max information (excess entropy in single cell data) to min.
 #' @slot informative_variants A character vector listing the most informative variants, used for downstream clustering analysis.
 #' @keywords genotype vcf vep variants
-#' @exportClass
+#' @export
 #' @examples
 #' genotypes <- ReadVcf("Myfolder/MyVcf.vcf")
 #' genotypes # Preview the contents of the genotype object
@@ -51,6 +51,29 @@ GenotypeObject <- setClass("genotype", slots=list(matrix="dgCMatrix",
                                                         variants_by_information="character",
                                                         informative_variants="character",
                                                         vartrix="list"))
+
+#' Create a genotype object
+#'
+#' @param matrix A dgCMatrix containing genotype data in vartrix conventions.
+#' @param metadata A data.frame containing variant metadata.
+#' @param variants A character vector listing the variants described in the object, in the same order as the rows of the matrix and metadata slots.
+#' @param vartrix A list of vartrix matrices
+#' @param variants_by_coverage A character vector listing the variants sorted from max coverage (number of cells in which there is data) to min.
+#' @param variants_by_information A character vector listing the variants sorted from max information (excess entropy in single cell data) to min.
+#' @param informative_variants A character vector listing the most informative variants, used for downstream clustering analysis.
+#' @return Returns a genotype object.
+#' @keywords genotype vcf vep variants
+#' @export
+CreateGenotypeObject <- function(matrix=NULL, metadata=NULL, vartrix=NULL, variants=NULL){
+  if(is.null(variants)){
+    if(rownames(matrix)!=rownames(metadata)){
+      warning("The row names in the matrix and metadata do not match, and list of variants is not given. Aborting.")
+    }else{
+      variants <- rownames(matrix)
+    }
+  }
+  return(GenotypeObject(matrix=matrix,metadata=metadata,vartrix=vartrix,variants=variants))
+}
 
 #' @describeIn GenotypeObject Show a summary of the contents of a genotype object.
 #' @export
