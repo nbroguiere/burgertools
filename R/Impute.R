5009c4feea12fa3067a8fb20b16b63a0f5ee594b
Nicolas Broguiere <broguiere.nicolas@gmail.com>
2022-07-23 06:25:37 +0200

    Impute gives a warning if the default assay is not RNA.

--- R/Impute.R
+++ R/Impute.R
@@ -2,7 +2,7 @@
 #'
 #' This function is a wrapper of the MAGIC imputation function to easily impute Seurat objects containing scRNA-seq data, with reasonable default parameters when used in the context of signature scoring for cell classification: only imputing the most variable genes and additional features of interest, and using only 40 PCs for efficiency, and using a narrow neighborhood to avoid over-smoothing.
 #' @param object Seurat object. Must contain an RNA assay with a data slot that will be used for imputation.
-#' @param features A subset of features/genes on which to run the imputation. Can be a list of signatures (Default: NULL/none, i.e. only impute all variable features if append.variable.features=TRUE as set by default).
+#' @param features A subset of features/genes on which to run the imputation. Can be a list of signatures in which the features in the list will be added, or can be "all" (Default: NULL/none, i.e. only impute all variable features if append.variable.features is TRUE, as set by default).
 #' @param append.variable.features Whether to append the variable features of the active assay in the Seurat object to the list of features to impute (Default: TRUE)
 #' @param npca Number of principal components to use for imputation.
 #' @param knn Number of nearest neighbors on which to compute bandiwth imputation.
@@ -19,6 +19,19 @@
 #' MySeuratObject <- Impute(MySeuratObject)
 #' MySeuratObject[["imputed.RNA"]]
 Impute <- function(object, features=NULL, append.variable.features=TRUE, npca=40, knn=3, t=2, n.jobs=6, assay.use="RNA", slot.use="data", name=paste0("imputed.",assay.use)){
+
+  backup_default_assay <- DefaultAssay(object)
+  DefaultAssay(object) <- assay.use
+
+  if(assay.use=="RNA" & DefaultAssay(object)!="RNA"){
+    cat("Beware that the imputed assay is RNA, even though the default assay is currently ",DefaultAssay(object),"\n")
+  }
+
+  if(features[1]=="all"){
+    cat("Imputing all features.\n")
+    features <- rownames(object)
+  }
+
   if(!is.null(features)){
     missing.features <- setdiff(unique(unlist(features)), rownames(object))
     features <- intersect(unique(unlist(features)), rownames(object))
@@ -26,21 +39,22 @@ Impute <- function(object, features=NULL, append.variable.features=TRUE, npca=40
       cat(paste("The following features were not found in the Seurat object and were omitted: \n",paste(missing.features,collapse=" "),"\n\n"))
     }
   }
+
   if(append.variable.features){
     if(length(VariableFeatures(object))){
       features <- unique(c(features,VariableFeatures(object)))
     }else{
-      warning("Variable features required for imputation, but not present in the Seurat object. Imputation not computed. To run imputation on user-defined features only, set append.variable.features to FALSE.")
-      return(object)
+      stop("Variable features required for imputation, but not present in the Seurat object. Imputation not computed. To run imputation on user-defined features only, set append.variable.features to FALSE.")
     }
   }
+
   if(length(features)>0){
     imputed <- Rmagic::magic(data = t(as.matrix(slot(object@assays[[assay.use]],slot.use)[features,])), npca=npca, knn=knn, t=t, n.jobs=n.jobs)
     imputed <- t(imputed$result)
     object[[name]] <- CreateAssayObject(imputed)
+    DefaultAssay(object) <- backup_default_assay
     return(object)
   }else{
-    warning("None of the features required were found in the Seurat object. Imputation not computed.")
-    return(object)
+    stop("None of the features required were found in the Seurat object. Imputation not computed.")
   }
 }
